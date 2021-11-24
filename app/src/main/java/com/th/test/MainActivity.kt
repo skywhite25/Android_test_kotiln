62b87ddab7a755a7311a747dbe13bdc982084bd8
TaeHun <skywhite25@naver.com>
2021-11-24 17:45:30 +0900

    11.24 마지막 참가자의 경우 다음 버튼이 아닌 결과 버튼을 구현하는 중 현재 다음 -> 결과 버튼 구현 성공 / 하지만 다음 버튼을 눌러야 결과 버튼이 출력 / 시행 필요

--- app/src/main/java/com/th/test/MainActivity.kt
+++ app/src/main/java/com/th/test/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.th.test
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -16,34 +17,62 @@ class MainActivity : AppCompatActivity() {
     var k = 1
     // 참가자들의 점수 리스트
     val point_list = mutableListOf<Float>()
+    // 블라인드 모드
+    var isBlind = true
 
+    // activity_start.xml 실행 함수
     fun start(){
         setContentView(R.layout.activity_start)
-        val tv_pnum : TextView = findViewById(R.id.tv_pnum)
+        val tv_pnum : TextView = findViewById(R.id.tv_random)
         val btn_m : Button = findViewById(R.id.btn_minus)
         val btn_p : Button = findViewById(R.id.btn_plus)
+        val btn : Button = findViewById(R.id.btn_start)
 
         tv_pnum.text = p_num.toString()
+
+        // - 버튼을 누르면 일어나는 일
+        btn_m.setOnClickListener{
+            // 숫자가 감소
+            p_num --
+            // 숫자가 1 미만으로 내려가지 않음
+            if(p_num == 0) {
+                p_num = 1
+            }
+            tv_pnum.text = p_num.toString()
+        }
+        // + 버튼을 누르면 일어나는 일
+        btn_p.setOnClickListener{
+            p_num ++
+            // 숫자가 4 이상으로 올라가지 않음음
+           if(p_num == 10) {
+                p_num = 9
+            }
+            tv_pnum.text = p_num.toString()
+        }
+        // 시작 버튼을 누르면 main() 함수가 출력
+        btn.setOnClickListener{
+            main()
+        }
     }
 
-
+    // activity_main.xml 실행 함수
     fun main(){
         setContentView(R.layout.activity_main)
 
         var timerTask : Timer? = null
         var stage = 1
         var sec : Int = 0
-        val tv : TextView = findViewById(R.id.tv_timer)
-        val tv_t : TextView = findViewById(R.id.tv_pnum)
+        val tv_pnum : TextView = findViewById(R.id.tv_random)
+        val tv_t : TextView = findViewById(R.id.tv_timer)
         val tv_p : TextView = findViewById(R.id.tv_point)
         val tv_people : TextView = findViewById(R.id.tv_people)
-        val btn : Button = findViewById(R.id.btn_main)
+        val btn : Button = findViewById(R.id.btn_start)
         // 버튼을 클릭하면 1~10 사이에 숫자 중 랜덤한 숫자 출력
         val random_box = Random()
         val num = random_box.nextInt(1001)
 
         // 목표 숫자 출력
-        tv.text = (num.toFloat()/100).toString()
+        tv_pnum.text = (num.toFloat()/100).toString()
         btn.text = "시작"
         tv_people.text = "참가자 $k"
 
@@ -58,13 +87,22 @@ class MainActivity : AppCompatActivity() {
                 timerTask = kotlin.concurrent.timer(period = 10) {
                     sec++
                     runOnUiThread {
-                        // 100분의 1로 나눈 int sec는 소수점이 나올 수 없으므로
-                        // double이나 float 타입으로 캐스팅해서 출력
-                        tv_t.text = (sec.toDouble() / 100).toString()
+                        // 블라인드 모드가 꺼져있을때
+                        if(isBlind == false) {
+                            // 100분의 1로 나눈 int sec는 소수점이 나올 수 없으므로
+                            // double이나 float 타입으로 캐스팅해서 출력
+                            tv_t.text = (sec.toDouble() / 100).toString()
+                        // 블라인드 모드가 켜져있을때
+                                // 블라인드 모드가 켜져있고 스테이지는 2단계
+                        } else if (isBlind == true && stage == 2){
+                            tv_t.text = " ?.??" // 시간이 보이지 않음
+                        }
                     }
                 }
                 btn.text = "정지"
             } else if (stage == 3) {
+                // 블라인드 모드이더라도 정지를 누르면 시간이 나오도록 처리
+                tv_t.text = (sec.toDouble() / 100).toString()
                 timerTask?.cancel()
                 // 랜덤값과 버튼을 눌러서 나온 값의 차이를 출력
                 val point = abs(sec - num).toFloat() / 100
@@ -75,23 +113,78 @@ class MainActivity : AppCompatActivity() {
                 tv_p.text = point.toString()
                 btn.text = "다음"
                 stage = 0
-            } else if (stage == 1){
-                // k번째 참가자 => k가 최대 참가자 수와 같거나 작을 경우 다시 반복
-                // 아닐 경우 반복이 되지 않음
-                if(k<p_num) {
+            }else if(k<p_num){
+                val point = abs(sec - num).toFloat() / 100
+                    tv_p.text = point.toString()
+                    btn.text = "다음"
                     k++
+//                    stage = 0
                     main()
-                } else {
-                    println(point_list)
+                     // 다음이 출력되는 stage 3 때 처음으로 돌아가도록 stage=0으로 설정
+            } else {
+                val point = abs(sec - num).toFloat() / 100
+                tv_p.text = point.toString()
+                btn.text = "결과"
+                btn.setOnClickListener{
+                    end()
                 }
             }
+//                if(k>=p_num){
+//                                        btn.text = "결과"
+//                    btn.setOnClickListener{
+//                        end()
+//                    }
+//                }
+//                tv_p.text = point.toString()
+//                btn.text = "다음"
+//                k++
+//                stage = 0
+//                main()
+//            }
+//            else if (stage == 1){
+//                // k번째 참가자 => k가 최대 참가자 수보다 작을 경우 다시 반복
+//                if(k<p_num) {
+//                    k++
+//                    main()
+////                // 아닐 경우 반복이 되지 않음
+//                }
+//                else {
+//                    btn.text = "결과"
+//                    btn.setOnClickListener{
+//                        end()
+//                    }
+//                }
+//            }
         }
     }
-        // 위의 main() 함수 실행
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
+
+    // activity_end.xml 실행 함수
+    @SuppressLint("SetTextI18n")
+    fun end() {
+        setContentView(R.layout.activity_end)
+        val tv_last: TextView = findViewById(R.id.tv_last)
+        val tv_lpoint: TextView = findViewById(R.id.tv_lpoint)
+        val btn_init: TextView = findViewById(R.id.btn_init)
+
+        // 꼴찌의 점수 출력 -> maxOrNull() : 점수 리스트에서 가장 큰 수 출력
+        tv_lpoint.text = (point_list.maxOrNull()).toString()
+        // 점수 리스트에 가장 큰 수를 가진 순위의 참가자 출력
+        var index_last = point_list.indexOf(point_list.maxOrNull())
+        tv_last.text = "참가자 " + (index_last+1).toString()
+
+        btn_init.setOnClickListener{
+            point_list.clear()
+            k = 1
             start()
         }
+    }
+
+    // 위의 main() 함수 실행
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        start()
+//      main()
+    }
 
 }
 
